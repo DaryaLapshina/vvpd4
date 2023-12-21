511e1543ea82ccff103872ccfbf2170d3bca982e
Darya <dashalaphina1810@gmail.com>
2023-12-21 20:20:33 +0700

    deadline_score

--- data/data.py
+++ data/data.py
@@ -16,3 +16,33 @@ def check_data(data):
             return "Wrong"
     else:
         return "Wrong"
+
+
+def convertor(arr):
+    keys = [arr[i].split(':')[0] for i in range(len(arr))]
+    values = [arr[i].split(':')[1] for i in range(len(arr))]
+    dictionary = dict(zip(keys, values))
+    return dictionary
+
+
+def deadline_score(pass_date, deadline_date):
+    pass_date = check_data(pass_date)
+    deadline_date = check_data(deadline_date)
+    print(pass_date)
+    if pass_date == "Wrong" or deadline_date == "Wrong":
+        return "Введённые данные неверны. Формат ввода: DD.MM.YYYY"
+    else:
+        if deadline_date >= pass_date:
+            days = (deadline_date - pass_date).days
+        else:
+            days = (pass_date - deadline_date).days * (-1)
+        if days >= 0:
+            return 5
+        elif -28 <= days < 0:
+            days = days * (-1)
+            for i in range(1, 5):
+                if (i - 1) * 7 < days <= i * 7:
+                    return 5 - i
+        else:
+            return 0
+
